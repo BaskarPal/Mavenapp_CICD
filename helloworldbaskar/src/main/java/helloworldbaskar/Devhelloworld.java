1b7c7b203843f5be0b9169541cd84e124df356b0
BaskarPal <90125493+BaskarPal@users.noreply.github.com>
2021-12-31 05:30:20 +0000

    Add files via upload
    From local

---
+++ helloworldbaskar/src/main/java/helloworldbaskar/Devhelloworld.java
@@ -0,0 +1,15 @@
+package helloworldbaskar;
+
+import java.util.Scanner;
+
+public class  Devhelloworld
+
+{
+public  Devhelloworld () {
+	System.out.println ("Enter your Name");
+	Scanner Input = new Scanner (System.in);
+
+	System.out.println("Welcome and Thankyou");
+	System.out.println ( Input.nextLine());
+}
+}
---
+++ helloworldbaskar/src/test/java/helloworldbaskar/Testhelloworld.java
@@ -0,0 +1,21 @@
+package helloworldbaskar;
+
+import java.util.Scanner;
+
+import org.junit.Test;
+
+public class Testhelloworld {
+	
+	/*@Test
+	public void Testhelloworld() {
+		System.out.println("...HelloWorldBaksar!...");
+	}*/
+	@Test
+	public void TestDevhellworld() {
+		System.out.println ("Enter your Name");
+		Scanner Input = new Scanner (System.in);
+		System.out.println ( Input.nextLine());
+		System.out.println("Welcome and Thankyou");
+		
+	}
+}
